a8b812bcdfddd8e49934a60133e37d6fc648b5ef
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-03 06:23:46 +0000

    Refactor how string is built, so its a ActiveSupport::SafeBuffer.new rather than a string

--- lib/action_view/helpers/dynamic_form.rb
+++ lib/action_view/helpers/dynamic_form.rb
@@ -234,12 +234,12 @@ module ActionView
               end
             end.inject(:+).join.html_safe
 
-            contents = ''
+            contents = ''.html_safe
             contents << content_tag(options[:header_tag] || :h2, header_message) unless header_message.blank?
             contents << content_tag(:p, message) unless message.blank?
             contents << content_tag(:ul, error_messages)
 
-            content_tag(:div, contents.html_safe, html)
+            content_tag(:div, contents, html)
           end
         else
           ''
